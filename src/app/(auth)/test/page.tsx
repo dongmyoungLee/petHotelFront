95cad17e3315a75bc929520c61b34d63270ab5a0
dongmyounglee <pajang1515@daum.net>
2025-03-18 15:27:52 +0900

    init

---
+++ src/app/(auth)/test/page.tsx
@@ -0,0 +1,5 @@
+export default function TestPage() {
+    return (
+        <><p>TEST</p></>
+    )
+}
---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+
+import mainLogo4 from '@/assets/2402232.webp';
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <>
+      안녕하세용Marketing
+          <div>
+              <Button>Click me</Button>
+          </div>
+      </>
+
+      // <Image src={mainLogo4} alt="Company Logo" priority />
+  );
+}
