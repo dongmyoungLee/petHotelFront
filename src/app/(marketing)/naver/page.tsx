9efb1cfaf790c478aa35de413936b15232bce8a3
dongmyounglee <pajang1515@daum.net>
2025-03-24 13:16:24 +0900

    oauth naver 추가

--- src/app/(marketing)/naver/page.tsx
+++ src/app/(marketing)/naver/page.tsx
@@ -1,23 +1,21 @@
 'use client';
-
 import {useRouter, useSearchParams} from "next/navigation";
 import {useEffect} from "react";
-import {google} from "@/app/api/auth/auth";
+import {google, naver} from "@/app/api/auth/auth";
 
-export default function Google() {
+export default function Naver() {
     const searchParams = useSearchParams();
     const router = useRouter();
     const code = searchParams.get('code');
 
     useEffect(() => {
-        google(code)
+        naver(code)
             .then((res) => {
                 router.push('/test');
             }).catch((err) => {
             console.log(err);
         })
     }, []);
-
     return (
         <h2>로그인 중 입니다..</h2>
     );
