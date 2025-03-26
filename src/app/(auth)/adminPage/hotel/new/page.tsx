bdb055d19845c0828d8decfdbb887482b233f7a5
dongmyounglee <pajang1515@daum.net>
2025-03-26 09:13:25 +0900

    호텔 조회 및 호텔 추가 템플릿 추가

---
+++ src/app/(auth)/adminPage/hotel/new/page.tsx
@@ -0,0 +1,5 @@
+export default function HotelAddPage() {
+    return (
+      <p>호텔추가페이지</p>
+    );
+}
---
+++ src/app/(auth)/adminPage/hotel/page.tsx
@@ -0,0 +1,5 @@
+export default function HotelPage() {
+    return (
+        <p>호텔 조회 페이지</p>
+    );
+}
