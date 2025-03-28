de9ee1c17d58f2c9deb26894d1212091f96087c4
dongmyounglee <pajang1515@daum.net>
2025-03-28 22:22:12 +0900

    Oauth 사용자도 Zustand - LocalStorage에 기본정보 저장..

--- src/app/(marketing)/(oauth)/kakao/page.tsx
+++ src/app/(marketing)/(oauth)/kakao/page.tsx
@@ -3,21 +3,31 @@
 import {useRouter, useSearchParams} from "next/navigation";
 import {useEffect} from "react";
 import {kakao} from "@/app/api/auth/user/auth";
+import {UserInfo} from "@/types/auth/user/authType";
+import {useStore} from "@/hooks/useStore";
+import {useAdminInfo, useUserInfo} from "@/hooks/useUserInfo";
+import {AxiosResponse} from "axios";
 
 export default function KaKao() {
     const searchParams = useSearchParams();
     const router = useRouter();
     const code = searchParams.get('code');
+    const { setUserInfo } = useUserInfo();
 
     useEffect(() => {
         kakao(code)
-            .then((res) => {
-                console.log(res)
+            .then((res: UserInfo) => {
+                setUserInfo({
+                    role: res.role,
+                    id: res.id,
+                    email: res.email,
+                    userName: res.userName,
+                });
                 router.push('/test');
             }).catch((err) => {
                 window.alert("인증 정보가 올바르지 않거나 잘못된 접근입니다.");
-            router.push('/');
-        })
+                router.push('/');
+            })
     }, [code]);
 
     return (
